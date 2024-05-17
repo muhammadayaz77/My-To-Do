cfa9ef85d7a66bf9869f568db706ccc435fb0330
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-05-17 15:56:36 +0500

    changes

--- script.js
+++ script.js
@@ -2,23 +2,32 @@ let input = document.querySelector('#in');
 let ul = document.querySelector('ul');
 
 document.querySelector('.btn').onclick = () => {
-    let li = document.createElement('li');
-    li.textContent = input.value;
-    ul.appendChild(li);
-
-    let span = document.createElement('span');
-    span.textContent = '\u00d7';
-    li.appendChild(span);
-
-    ul.onclick = (e) => {
-        if(e.target.tagName == 'SPAN')
-            {
-                e.target.parentElement.remove();
+    if(input.value == '')
+        {
+            alert('Add something')
+        }
+        else
+        {
+            let li = document.createElement('li');
+            li.textContent = input.value;
+            ul.appendChild(li);
+            
+        
+            let span = document.createElement('span');
+            span.textContent = '\u00d7';
+            li.appendChild(span);
+        
+            ul.onclick = (e) => {
+                if(e.target.tagName == 'SPAN')
+                    {
+                        e.target.parentElement.remove();
+                    }
+                else if(e.target.tagName == "LI")
+                    {
+                        e.target.classList.toggle('liCon')
+                    }
             }
-        else if(e.target.tagName == "LI")
-            {
-                e.target.classList.toggle('liCon')
-            }
-    }
-    input.value = ''
+            input.value = ''
+        }
+   
 }
