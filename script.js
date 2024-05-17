199623360d9eabc37f9076d6c5f300c76a42485c
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-05-17 15:51:57 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let input = document.querySelector('#in');
+let ul = document.querySelector('ul');
+
+document.querySelector('.btn').onclick = () => {
+    let li = document.createElement('li');
+    li.textContent = input.value;
+    ul.appendChild(li);
+
+    let span = document.createElement('span');
+    span.textContent = '\u00d7';
+    li.appendChild(span);
+
+    ul.onclick = (e) => {
+        if(e.target.tagName == 'SPAN')
+            {
+                e.target.parentElement.remove();
+            }
+        else if(e.target.tagName == "LI")
+            {
+                e.target.classList.toggle('liCon')
+            }
+    }
+    input.value = ''
+}
